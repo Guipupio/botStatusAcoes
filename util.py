1caf4ed2f1a2d46e492cdec379e7294b1e0119d8
pupio <guicpupio@gmail.com>
2019-05-01 14:54:27 -0300

    Comentario nas funcoes

--- util.py
+++ util.py
@@ -9,9 +9,15 @@ from selenium import webdriver
 from time import sleep
 from os import getcwd as curPath
 from tkinter import filedialog, Tk
+from getpass import getpass
 
-
-def login_rico(browser, user:str, password:str):
+def login_rico(browser, user:str, password=getpass()):
+    """
+    INPUT: 
+        browser: webdriver.Chrome
+        user (str): usuario para login
+        password (str: optional): senha para login
+    """
     insereTextoLento(browser, r'//*[@id="loginForm"]/div[1]/input',  user)
     browser.find_element_by_xpath(r'//*[@id="loginForm"]/button').submit()
     # Aguarda 5 segundos a pagina renderizar
@@ -58,19 +64,32 @@ def get_path_janela_dialogo(initPath = None, multiplos_arquivos= False, titulo='
         
     
 def initBrowser(url:str, chromeDriverPath=curPath() + r'/chromedriver'):
+    """
+    INPUT:
+        url (str): Pagina que o browser deve abrir
+        chromeDriverPath (str): Path ate o chromedriver, Default: path atual
+    """
     # Gera browser
     browser = webdriver.Chrome(executable_path=chromeDriverPath)
     # Acessa link contendo links para as empresas
     browser.get(url)
     return browser
 
-def insereTextoLento(browser: webdriver.Chrome, xpath_texto: str, texto: str):
+def insereTextoLento(browser: webdriver.Chrome, xpath_texto: str, texto: str, delay_escrita = 0.08, delay_retorno = 2.5):
+    """
+    INPUT:
+        browser: webdriver.Chrome
+        xpath_texto(str): xpath do local onde o texto sera inserido
+        texto (str): Texto que deve ser inserido
+        delay_escrita (float): tempo de espera entre digitar cada caracter, Default: 0.08
+        delay_retorno (float): tempo de espera para retornar, apos digitar tds os caracteres, Default: 2.5
+    """
     browser.find_element_by_xpath(xpath_texto).clear()
     # Inserimos os caracteres com Delay, pq o site nao reconhece quando escrevemos muito rapido
     for char in texto:
         browser.find_element_by_xpath(xpath_texto).send_keys(char)
-        sleep(0.08)
-    sleep(2.5)
+        sleep(delay_escrita)
+    sleep(delay_retorno)
     
 
 
