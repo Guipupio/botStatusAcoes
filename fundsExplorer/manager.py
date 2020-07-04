0e32555a332eafe9d8084c2443b3e2082061500a
Guilherme Pupio <guicpupio@gmail.com>
2020-07-04 17:01:02 -0300

    Criacao de graficos por setor -> OK

--- fundsExplorer/manager.py
+++ fundsExplorer/manager.py
@@ -1,5 +1,8 @@
 from browser.browsers import ChromeBrowser
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import math
 
 def generate_csv_current_data(path_save_csv: str = None) -> None:
     # Instancia o Browser
@@ -18,3 +21,51 @@ def generate_csv_current_data(path_save_csv: str = None) -> None:
 
     # Fecha Browser
     browser.quit()
+
+def analisa_fiis(path_to_csv: str):
+    # Obtem data Frame
+    df = pd.read_csv(path_to_csv)
+    
+    # obtem apenas os p/vpa positivos
+    df_filtrado = df[df['p/vpa'] > 0]
+    
+    # Obtem os setores dos FIIS
+    list_setores = df_filtrado.setor.unique()
+    num_setores = len(list_setores)
+    isqrt_num_setores = math.isqrt(num_setores)
+    
+    num_plots_vertical = num_plots_horizontal = isqrt_num_setores
+    
+    # Adicionamos mais uma linha nos plots
+    if  isqrt_num_setores ** 2 != num_setores:
+        num_plots_vertical += 1
+    
+    # Gera subplot das dimensoes necessarias
+    fig, subplots = plt.subplots(num_plots_vertical, num_plots_horizontal,  figsize=(20,10), dpi=200)
+    
+    for idx, setor in enumerate(list_setores):
+        
+        # Filtra por setor
+        _df = df_filtrado[df_filtrado.setor == setor]
+        
+        # obtem dados do Setor
+        y = _df['dy (12m)\nmédia']
+        x = _df['p/vpa']
+
+        # Define indices do subplot
+        idx_v = int(idx/num_plots_horizontal)
+        idx_h = idx % num_plots_horizontal
+
+        # Plota FIIs
+        subplots[idx_v][idx_h].scatter(x, y)
+
+        # Labels do subplot
+        subplots[idx_v][idx_h].set_title(setor)
+        subplots[idx_v][idx_h].set_xlabel("P/VPA")
+        subplots[idx_v][idx_h].set_ylabel("DY (12M) Médio")
+        
+    fig.tight_layout(pad=3.0)
+    fig.suptitle('Análise FIIs por Setores', fontsize=16)
+    
+    fig.savefig("Analise.png")
+    plt.show()
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from argparse import ArgumentParser
 from datetime import datetime
+from os import getcwd as pwd
+from os import listdir
 
-from fundsExplorer.manager import generate_csv_current_data
+from fundsExplorer.manager import analisa_fiis, generate_csv_current_data
 
 parser = ArgumentParser()
+
 parser.add_argument("-gf", "--get-fiis", type=int, dest="get_fiis",
                     help="Busca Rank de FIIs no FundsExplorer (default: 0)", metavar='', default=0)
+
+parser.add_argument("-pf", "--plot-fiis", type=int, dest="plot_analise_fiis",
+                    help="Plota dos fiis (default: analise do ultimo csv obtido)", metavar='', default=0)
 
 parser.add_argument("-q", "--quiet",
                     action="store_false", dest="verbose", default=True,
@@ -14,6 +20,15 @@ parser.add_argument("-q", "--quiet",
 if __name__ == '__main__':
     args = parser.parse_args()
     
+    PATH_FIIS = '/'.join([pwd(), 'csvs', 'fiis'])
+    filename = datetime.now().strftime("%Y-%m-%d")
+
     if args.get_fiis:
         # Gera CSV dos dados do FundsExplorer
-        generate_csv_current_data(path_save_csv="{data}_fiis.csv".format(data=datetime.now().strftime("%Y-%m-%d")))
+        generate_csv_current_data(path_save_csv=f"{PATH_FIIS}/{filename}.csv")
+
+    if args.plot_analise_fiis:
+        filenames = listdir(PATH_FIIS)
+        filenames.sort()
+
+        analisa_fiis(filenames[-1])
